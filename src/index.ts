f45200d69dca7b06c5246b03c1adf8b75dd8a8dc
Rohan <rohansalunkhe700@gmail.com>
2026-02-15 01:33:39 +0530

    broadcast logic wrote basic

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,24 @@
 import { WebSocket, WebSocketServer } from "ws";
 
-const ws = new WebSocketServer({port: 8080});
+const wss = new WebSocketServer({port: 8080});
 
 let userCount = 0 ;
+let allSockets: WebSocket[] = [] ;
 
-ws.on("connection" , (socket) => {
+
+wss.on("connection" , (socket) => {
+
+    allSockets.push(socket); // storing all the sockets in an array to send message to 
+
     console.log("New client connected");
     userCount++;
     console.log(`Total users connected: ${userCount}`);
-
+    
     socket.on("message" , (message) => {
-
+        for(const socket of allSockets ) {
+            socket.send(message.toString() + ": message from the server");
+       }
         console.log(`Received message: ${message}`);
-        
+
     })
 })
