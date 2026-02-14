bd4ed8e501018059a5c603c7731e944d07dd5017
Rohan <rohansalunkhe700@gmail.com>
2026-02-14 17:22:10 +0530

    add dev script , websocketServer connection basic

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import { WebSocket, WebSocketServer } from "ws";
+
+const ws = new WebSocketServer({port: 8080});
+
+let userCount = 0 ;
+
+ws.on("connection" , (socket) => {
+    console.log("New client connected");
+    userCount++;
+    console.log(`Total users connected: ${userCount}`);
+})
