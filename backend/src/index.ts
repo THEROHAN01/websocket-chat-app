55ff0913a3805b7fa7285c0e5472ac2007156086
Rohan <rohansalunkhe700@gmail.com>
2026-02-15 13:07:11 +0530

    basic chat backend logic sorted

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,23 +2,40 @@ import { WebSocket, WebSocketServer } from "ws";
 
 const wss = new WebSocketServer({port: 8081});
 
+interface User {
+    socket : WebSocket ;
+    room : string;
+}
+
+
 let userCount = 0 ;
-let allSockets: WebSocket[] = [] ;
+let allSockets: User[] = [] ;
+
 
 
 wss.on("connection" , (socket) => {
-
-    allSockets.push(socket); // storing all the sockets in an array to send message to 
-
-    console.log("New client connected");
     userCount++;
     console.log(`Total users connected: ${userCount}`);
     
     socket.on("message" , (message) => {
-        for(const socket of allSockets ) {
-            socket.send(message.toString() + ": message from the server");
-       }
-        console.log(`Received message: ${message}`);
+        
+        const parsedMessage = JSON.parse(message.toString());
+        console.log(parsedMessage);
+        if(parsedMessage.type == "join"){
+            allSockets.push({
+                socket:  socket,
+                room: parsedMessage.payload.roomId
+            })
+        }
 
+        if(parsedMessage.type == "chat"){
+            const currentUserRoom = allSockets.find((x) => (x.socket) == socket )?.room
+
+            for(const user of allSockets){
+                if(user.room == currentUserRoom){
+                    user.socket.send(parsedMessage.payload.message)
+                }
+            }
+        }
     })
 })
